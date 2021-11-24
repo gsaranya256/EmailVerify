84af84ed82cfdd6b2bf0fb50b031f21531bb3619
Saranya <gsaranya256@gmail.com>
2021-11-24 16:57:17 +0530

    External API implementation done.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const emailValidator = require("email-verify");
 const ejs = require("ejs");
+const http = require("http");
 
 
 const app = express();
@@ -17,10 +18,38 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     console.log("POST req received");
     const firstName = req.body.fName;
+    // Disposable/Temp-mail = "vamimif889@mainctu.com";
     const email = req.body.email;
+    const access_key = "ce043f1c7a184ca11ebecd0eba33c69c";
+    const url = "http://apilayer.net/api/check?access_key="+access_key+"&email="+email+"&smtp=1&format=1"
     console.log("Name >>> " + firstName + " Email >>>> " + email);
+    http.get(url,function (response){
+        console.log(response.statusCode);
+        response.on("data", function (data) {
+            console.log(data);        
+            const emailStatus = JSON.parse(data);       //Converts the hexadecimal data to the Javascript object
+            console.log(emailStatus);
+            const username = emailStatus.user; //Use JSON VIewer Pro to get the path
+            const domain = emailStatus.domain;
+            const validFormat = emailStatus.format_valid;
+            const validEmail = emailStatus.mx_found;
+            const isRoleEmail = emailStatus.role;
+            const isFreeProvider = emailStatus.free;
+            const isTempEmail = emailStatus.disposable;
+            const qualityScore = emailStatus.score;
+            if(validFormat && validEmail){
+                res.render("valid",{email : email, username:username, domain : domain, validEmail:validEmail, isRoleEmail: isRoleEmail});
+            }
+            else{
+                res.render("invalid",{email : email, username:username, domain : domain, validEmail:validEmail,isRoleEmail: isRoleEmail});
+            }
+            
 
-    emailValidator.verify(email, function (err, info) {
+        
+        })
+    });
+
+   /* emailValidator.verify(email, function (err, info) {
 
 
         console.log("INFO >>>>> " + info.success);
@@ -30,19 +59,19 @@ app.post("/", function (req, res) {
 
         if (!err) {
             if (info.success) {
-                res.render("valid", { email:email, username:firstName, info: info.info });
+                res.render("valid", { email: email, username: firstName, info: info.info });
             }
             else {
-                res.render("invalid", { email:email, username:firstName, info: info.info });
+                res.render("invalid", { email: email, username: firstName, info: info.info });
             }
         }
         else {
-            res.render("invalid", { email:email, username:firstName, info: info.info });
+            res.render("invalid", { email: email, username: firstName, info: info.info });
         }
 
 
 
-    });
+    });*/
 
 });
 
